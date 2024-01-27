9a9dfa79cf9e248abac54b72a1385171de81067c
Jonathan Delgado <hi@jon.soy>
2024-01-27 20:17:55 -0300

    feat: add custom name option for demo creation
    The ability to assign custom names during the creation of demos has been
    implemented. This flexibility allows easier identification and
    management of demos. The default demo name generation using ulid() is
    still retained as a fallback for when no custom name is given, ensuring
    backward compatibility. This feature delivers an enhanced user
    experience as well as improved administrative management of demos.
    
    Please note: the new `--name` or `-n` CLI argument can be used to
    specify a custom name.

--- src/cmd/create.ts
+++ src/cmd/create.ts
@@ -1,4 +1,4 @@
-import { flag, flags, isBooleanAt, makeHelmMessage, rule, type Rule } from "@jondotsoy/flags";
+import { flag, flags, isBooleanAt, isStringAt, makeHelmMessage, rule, type Rule } from "@jondotsoy/flags";
 import { createWorkspace } from "../create_workspace";
 import { ulid } from "ulid";
 import { globalConfig } from "../common/load_local_config";
@@ -7,20 +7,24 @@ export const create = async (args: string[]) => {
   type Options = {
     removeWorkspaceAfter: boolean;
     showHelp: boolean
+    name: string
   };
 
   const rules: Rule<Options>[] = [
     rule(flag("--rm"), isBooleanAt("removeWorkspaceAfter"), {
       description: "remove the workspace before of use",
     }),
+    rule(flag('--name', '-n'), isStringAt('name'), { description: 'name for the demo' }),
     rule(flag('--help', '-h'), isBooleanAt('showHelp'), { description: 'show this message' }),
   ];
 
-  const { removeWorkspaceAfter, showHelp } = flags<Options>(args, {}, rules);
+  const { removeWorkspaceAfter, showHelp, name } = flags<Options>(args, {}, rules);
 
   if (showHelp) return console.log(makeHelmMessage('demo create', rules, ['', '--rm']))
 
-  const location = new URL(`${ulid()}/`, globalConfig.demosLocation);
+  const relativePath = `${name ?? ulid()}/`
+
+  const location = new URL(relativePath, globalConfig.demosLocation);
 
   await createWorkspace({
     location,
